779ca57a719910fbd76f984fc9d46947e289a7ac
Prajwal Vyavahare <122688935+Intex51@users.noreply.github.com>
2023-11-04 08:50:05 +0530

    Add files via upload
    this is csday1 .This is dotnet day1 tutorial from bootcamp first day.

---
+++ csday1/Program.cs
@@ -0,0 +1,49 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// using System.ComponentModel;
+// using System.Diagnostics.CodeAnalysis;
+// using System.IO.Compression;
+
+// //Program to do the sum
+
+// // Console.WriteLine("Hello, World!");
+// // int num1, num2 ;
+// // Console.WriteLine("What are the Num1 & Num2");
+// // num1=Convert.ToInt32(Console.ReadLine());
+// // num2=Convert.ToInt32(Console.ReadLine());
+// // int Sum= num1 + num2 ;
+// // Console.WriteLine($"The Sum of Given Inputs Are: {Sum}");
+
+// // //Table of given number
+int num;
+Console.WriteLine("The table which you want to write");
+num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num} x 1 ={num*1}");
+Console.WriteLine($"{num} x 2 ={num*2}");
+Console.WriteLine($"{num} x 3 ={num*3}");
+Console.WriteLine($"{num} x 4 ={num*4}");
+Console.WriteLine($"{num} x 5 ={num*5}");
+Console.WriteLine($"{num} x 6 ={num*6}");
+Console.WriteLine($"{num} x 7 ={num*7}");
+Console.WriteLine($"{num} x 8 ={num*8}");
+Console.WriteLine($"{num} x 9 ={num*9}");
+Console.WriteLine($"{num} x 10 ={num*10}");
+
+// Sum of given four digit number
+// Console.WriteLine("enter any four digit number");
+// int n =Convert.ToInt32(Console.ReadLine());
+// int rem=n%10;
+// int m=n/10;
+// int sum=rem;
+// Console.WriteLine("1 iteration:" +sum);
+// rem=m%10;
+// m= m/10;
+// sum=rem+sum;
+// Console.WriteLine("2 iteration:"+sum);
+// rem=m%10;
+// m= m/10;
+// sum= sum+rem;
+// Console.WriteLine("3 iteration:"+sum);
+// rem=m % 10 ;
+// m=m/10;
+// sum= sum + rem;
+// Console.WriteLine("the final sum is " +sum);
