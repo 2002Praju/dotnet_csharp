3e09efaf33c0712e233415a01ecd5a03038fcfb8
Prajwal Vyavahare <122688935+Intex51@users.noreply.github.com>
2023-11-04 08:53:55 +0530

    Add files via upload
    This is day 3 of dotnet bootcamp

---
+++ csday3/Dowhilejee.cs
@@ -0,0 +1,13 @@
+public class Dowhilejee{
+    public static void Main(){
+        int[] numbers = new int[] {10,20,30,40,50};
+         
+        foreach (var item in numbers){
+            Console.WriteLine(item);
+        }
+
+        foreach (var item in numbers){
+            Console.WriteLine(item);
+        }
+    }
+}
